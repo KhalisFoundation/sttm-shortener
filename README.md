754902f9c9547f8f2d9b1bff5ec29409be5f053c
sttmshort on use-ded01 <sttmshort@use-ded02.khalis.net>
2018-05-18 13:04:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sttm-shortener
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+$path = explode('/',$_GET['path']);
+$sttm = 'https://www.sikhitothemax.org';
+
+function go ($url) {
+	header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+	header('HTTP/1.1 301 Moved Permanently');
+	header('Location: ' .  $url);
+	exit;
+}
+
+function goAng ($n) {
+	global $sttm;
+	is_numeric($n)? go("$sttm/ang?ang=$n&source=G") : go($sttm);
+}
+
+function goSearch ($q) {
+	global $sttm;
+	go("$sttm/search?q=$q");
+}
+
+function goShabad ($n) {
+	global $sttm;
+	is_numeric($n)? go("$sttm/shabad?id=$n") : go($sttm);
+}
+
+switch($path[0]) {
+	case 'a':
+		goAng($path[1]);
+		break;
+	case 'h':
+		go("$sttm/hukamnama");
+		break;
+	case 'q':
+		goSearch($path[1]);
+		break;
+	case 'r':
+		go("$sttm/random");
+		break;
+	case 's':
+		goShabad($path[1]);
+		break;
+	default:
+		go($sttm.$_SERVER['REQUEST_URI']);
+		break;
+}
+
